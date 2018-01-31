a9e05adf599f3b847518b01a626a7779bd07d943
LAPTOP-9PJ03RE0\Daniel <daniel.schitz@web.de>
2018-01-31 13:34:14 +0100

    drawer frontend rdy

--- AppALK/app/src/main/java/com/example/kai/appalk/HomeScreen.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/HomeScreen.java
@@ -102,27 +102,35 @@ public class HomeScreen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera)
+        if (id == R.id.faq)
         {
             // Handle the camera action
         }
-        else if (id == R.id.nav_gallery)
+        else if (id == R.id.news)
         {
 
         }
-        else if (id == R.id.nav_slideshow)
+        else if (id == R.id.rabatte)
         {
 
         }
-        else if (id == R.id.nav_manage)
+        else if (id == R.id.kongresse)
         {
 
         }
-        else if (id == R.id.nav_share)
+        else if (id == R.id.nebenwirkungen)
         {
 
         }
-        else if (id == R.id.nav_send)
+        else if (id == R.id.fachinfos)
+        {
+
+        }
+        else if (id == R.id.einstellungen)
+        {
+
+        }
+        else if (id == R.id.abmelden)
         {
 
         }
