f6870a03e69654d668b257d11979ceebbc75ad21
AerandirGames <k.richter.1996@web.de>
2018-01-31 13:49:53 +0100

    zweiter commit

--- AppALK/app/src/main/java/com/example/kai/appalk/Registrieren_Tab1.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Registrieren_Tab1.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Kai on 29.01.2018.
@@ -16,6 +17,16 @@ public class Registrieren_Tab1 extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_tab1_doccheck, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab1_doccheck, container, false);
+        Button button = (Button) view.findViewById(R.id.but_tab1weiter);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                
+            }
+        });
+        return view;
     }
 }
