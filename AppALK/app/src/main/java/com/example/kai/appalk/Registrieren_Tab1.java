0250709e316d39e5a6008b35820d8de19d452ed6
AerandirGames <k.richter.1996@web.de>
2018-01-31 16:31:41 +0100

    dritter commit

--- AppALK/app/src/main/java/com/example/kai/appalk/Registrieren_Tab1.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Registrieren_Tab1.java
@@ -3,6 +3,7 @@ package com.example.kai.appalk;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +19,24 @@ public class Registrieren_Tab1 extends Fragment
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_tab1_doccheck, container, false);
-        Button button = (Button) view.findViewById(R.id.but_tab1weiter);
+        Button button = view.findViewById(R.id.but_tab1weiter);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                
+                nextFragment();
             }
         });
         return view;
+    }
+
+    public void nextFragment ()
+    {
+        Registrieren_Tab2 tab2 = new Registrieren_Tab2();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.tabItem2, tab2);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
