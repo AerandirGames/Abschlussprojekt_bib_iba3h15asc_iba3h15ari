aea8878d29f1be0e9dc1b995c3949f3e71ebd949
LAPTOP-9PJ03RE0\Daniel <daniel.schitz@web.de>
2018-02-01 13:27:00 +0100

    Klassen erstellt

--- AppALK/app/src/main/java/com/example/kai/appalk/DatenbankManager.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/DatenbankManager.java
@@ -1,8 +1,28 @@
 package com.example.kai.appalk;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Daniel on 31.01.2018.
  */
 
-public class DatenbankManager {
+public class DatenbankManager extends SQLiteOpenHelper{
+
+    
+
+    @Override
+        public void onCreate(SQLiteDatabase db)
+        {
+
+        }
+
+    @Override
+    public void onUpgradee(SQLiteDatabase db, int i, int i2)
+    {
+
+    }
+
 }
+
+
