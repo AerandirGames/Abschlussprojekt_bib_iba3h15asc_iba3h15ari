cd5aa2df9335e2484926f9a9ef316e20f3714a7a
AerandirGames <k.richter.1996@web.de>
2018-02-14 10:57:36 +0100

    no message

--- AppALK/app/src/main/java/com/example/kai/appalk/Rabatte.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Rabatte.java
@@ -1,8 +1,15 @@
 package com.example.kai.appalk;
 
-/**
- * Created by Daniel on 31.01.2018.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Rabatte {
+public class Rabatte extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rabatte);
+    }
 }
