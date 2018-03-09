c6891d6418e72816e22424a4cc42bd149ec4f8b4
LAPTOP-9PJ03RE0\Daniel <daniel.schitz@web.de>
2018-03-09 13:42:38 +0100

    jhb

--- AppALK/app/src/main/java/com/example/kai/appalk/Kontakt.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Kontakt.java
@@ -6,8 +6,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Kontakt extends HomeScreen
+        implements AdapterView.OnItemSelectedListener
 {
 
     @Override
@@ -36,5 +42,41 @@ public class Kontakt extends HomeScreen
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ;
+
+
+        TextView kontaktanzeige = findViewById(R.id.kontaktanzeige);
+        Spinner kontaktdaten = findViewById(R.id.kontaktdaten);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.kontaktArray, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        kontaktdaten.setAdapter(adapter);
+
+        kontaktdaten.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
