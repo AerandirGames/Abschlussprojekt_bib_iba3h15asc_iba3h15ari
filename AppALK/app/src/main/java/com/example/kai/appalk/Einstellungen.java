e07c75787bcf789144da5b45e3bdd27b9503526f
AerandirGames <k.richter.1996@web.de>
2018-03-19 11:19:04 +0100

    no message

--- AppALK/app/src/main/java/com/example/kai/appalk/Einstellungen.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Einstellungen.java
@@ -1,11 +1,15 @@
 package com.example.kai.appalk;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Einstellungen extends AppCompatActivity
+public class Einstellungen extends HomeScreen
 {
 
     @Override
@@ -14,11 +18,26 @@ public class Einstellungen extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_einstellungen);
 
-    }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    public void zuEinstellungenNetzwerk (View view)
-    {
-        startActivity(new Intent(this, EinstellungenNetzwerk.class));
+        try
+        {
+            getSupportActionBar().setTitle("Einstellungen");
+        }
+        catch(NullPointerException n)
+        {
+            System.out.println("Nullpointer Exception");
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public void zuEinstellungenAccount (View view)
