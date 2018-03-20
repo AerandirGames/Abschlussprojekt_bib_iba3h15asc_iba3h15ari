6f4f7ce40cce723834e8632ea608b997e14c341d
LAPTOP-9PJ03RE0\Daniel <daniel.schitz@web.de>
2018-03-20 17:46:49 +0100

    sef

--- AppALK/app/src/main/java/com/example/kai/appalk/Kongressinfos.java
+++ AppALK/app/src/main/java/com/example/kai/appalk/Kongressinfos.java
@@ -2,17 +2,35 @@ package com.example.kai.appalk;
 
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 
 import com.google.zxing.Result;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class Kongressinfos extends HomeScreen implements ZXingScannerView.ResultHandler
 {
     private ZXingScannerView mScannerView;
+    ProgressDialog progressDialog;
+    Result myResult;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -40,15 +58,64 @@ public class Kongressinfos extends HomeScreen implements ZXingScannerView.Result
     @Override
     public void handleResult(Result result)
     {
+        myResult = result;
         Log.v("handleResult",result.getText());
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Scan result");
         builder.setMessage(result.getText());
+
+        progressDialog = new ProgressDialog(Kongressinfos.this);
+        progressDialog.setMessage("Datei wird heruntergeladen...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                downloadFile(myResult.getText().toString());
+            }
+        }).start();
 
         AlertDialog ad = builder.create();
         ad.show();
 
         //mScannerView
 
+    }
+
+    private void downloadFile(String a) {
+        try {
+            URL fileurl = new URL(a);
+            URLConnection uc = fileurl.openConnection();
+            uc.connect();
+            InputStream ic = new BufferedInputStream(uc.getInputStream(),8192);
+
+            File downloadordner = new File(Environment.getExternalStorageDirectory(),"Download");
+
+            if(!downloadordner.exists())
+            {
+                downloadordner.mkdirs();
+
+            }
+
+            final File downloadedFile = new File(downloadordner, "kongressbeispiel" + System.currentTimeMillis() + "pdf");
+
+            OutputStream outputStream = new FileOutputStream(downloadedFile);
+            byte[] buffer = new byte[1024];
+
+            int read;
+            while((read = ic.read(buffer))!= -1)
+            {
+                outputStream.write(buffer,0,read);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            ic.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
